b65b3023aacc2d5515a8b7a53a33f7dfadd409ba
Sakib Fakir <FakirSakib22232@gmail.com>
2025-08-17 17:39:13 +0600

    analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/next"
 import './../style/globals.css'
 
 
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
