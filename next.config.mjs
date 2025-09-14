1c87b19da41df84f145a9c17161e5c2acd238d90
Sakib Fakir <FakirSakib22232@gmail.com>
2025-09-14 12:30:38 +0600

    Add redirects for www and HTTPS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'quicksavevid.com' }],
+        destination: 'https://www.quicksavevid.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'header', key: 'x-forwarded-proto', value: 'http' }],
+        destination: 'https://www.quicksavevid.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
+module.exports = nextConfig;
